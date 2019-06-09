eb431f51f24f1c74685b8539769deb9c1d507369
PiotOw <powczarczyk@vp.pl>
2019-06-09 15:11:50 +0200

    OR gate fix

--- src/Core/WwBlueprints/OR.java
+++ src/Core/WwBlueprints/OR.java
@@ -10,7 +10,7 @@ public class OR extends Blueprint {
         int[] scheme = {0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0};
         for (int a = 0; a < 5; a++) {
             for (int b = 0; b < 9; b++) {
-                if (scheme[b + a * 8] == 1) {
+                if (scheme[b + a * 9] == 1) {
                     cellularAutomaton.setCellAtIndex(x + b, y + a, new Conductor());
                 } else {
                     cellularAutomaton.setCellAtIndex(x + b, y + a, new Empty());
