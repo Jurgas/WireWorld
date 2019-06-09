f701be49f1c309b3ea262b41852dd7a02d858317
PiotOw <powczarczyk@vp.pl>
2019-06-09 16:07:58 +0200

    Testy metod: drawBlueprint readFile

--- src/Testy/drawBlueprintTest.java
+++ src/Testy/drawBlueprintTest.java
@@ -1,4 +1,40 @@
 package Testy;
 
+import Core.CellularAutomaton;
+import Core.GameOfLife;
+import Core.GolCells.Alive;
+import Core.GolCells.Dead;
+
 public class drawBlueprintTest {
+    public static void main(String[] args) {
+        boolean success = false;
+        CellularAutomaton cellularAutomaton = new GameOfLife();
+        cellularAutomaton.createEmptyGrid(4, 4, cellularAutomaton);
+        cellularAutomaton.drawBlueprint(1,1 ,cellularAutomaton, "Glider");
+        if(cellularAutomaton.getCellAtIndex(0, 0) instanceof Dead &&
+                cellularAutomaton.getCellAtIndex(1, 0) instanceof Dead &&
+                cellularAutomaton.getCellAtIndex(2, 0) instanceof Dead &&
+                cellularAutomaton.getCellAtIndex(3, 0) instanceof Dead &&
+                cellularAutomaton.getCellAtIndex(0, 1) instanceof Dead &&
+                cellularAutomaton.getCellAtIndex(1, 1) instanceof Dead &&
+                cellularAutomaton.getCellAtIndex(2, 1) instanceof Alive &&
+                cellularAutomaton.getCellAtIndex(3, 1) instanceof Dead &&
+                cellularAutomaton.getCellAtIndex(0, 2) instanceof Dead &&
+                cellularAutomaton.getCellAtIndex(1, 2) instanceof Dead &&
+                cellularAutomaton.getCellAtIndex(2, 2) instanceof Dead &&
+                cellularAutomaton.getCellAtIndex(3, 2) instanceof Alive &&
+                cellularAutomaton.getCellAtIndex(0, 3) instanceof Dead &&
+                cellularAutomaton.getCellAtIndex(1, 3) instanceof Alive &&
+                cellularAutomaton.getCellAtIndex(2, 3) instanceof Alive &&
+                cellularAutomaton.getCellAtIndex(3, 3) instanceof Alive) {
+            success = true;
+        }
+        if(success){
+            System.out.println("Test metody drawBlueprint() zakończony powodzeniem!");
+        }
+        else{
+            System.out.println("Test metody drawBlueprint() zakończony niepowodzeniem!");
+        }
+
+    }
 }
--- src/Testy/readFileTest.java
+++ src/Testy/readFileTest.java
@@ -1,4 +1,43 @@
 package Testy;
 
+import Core.CellularAutomaton;
+import Core.InputOutput.Reader;
+import Core.WireWorld;
+import Core.WwCells.Empty;
+import Core.WwCells.Head;
+import Core.WwCells.Tail;
+
+import java.io.File;
+import java.io.IOException;
+
 public class readFileTest {
+
+
+    public static void main(String[] args) throws IOException {
+        boolean success = false;
+        CellularAutomaton readData = new WireWorld();
+        Reader reader = new Reader();
+        File data = new File("/Users/piotrowczarczyk/IdeaProjects/WireWorld2/cellgrids/data.txt");
+        reader.readFile(readData, data);
+
+        if(readData.getCellAtIndex(0, 0) instanceof Empty &&
+                readData.getCellAtIndex(1, 0) instanceof Head &&
+                readData.getCellAtIndex(2, 0) instanceof Tail &&
+                readData.getCellAtIndex(0, 1) instanceof Head &&
+                readData.getCellAtIndex(1, 1) instanceof Head &&
+                readData.getCellAtIndex(2, 1) instanceof Head &&
+                readData.getCellAtIndex(0, 2) instanceof Empty &&
+                readData.getCellAtIndex(1, 2) instanceof Empty &&
+                readData.getCellAtIndex(2, 2) instanceof Empty) {
+            success = true;
+        }
+
+        if(success){
+            System.out.println("Test metody readFile() zakończony powodzeniem!");
+        }
+        else{
+            System.out.println("Test metody readFile() zakończony niepowodzeniem!");
+        }
+    }
+
 }
